915af343d5a65b6a7733324a3b55eacfb7300826
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-05-27 15:26:41 +0200

    api

---
+++ main.js
@@ -0,0 +1,23 @@
+let searchButton = document.querySelector("#search");
+let content = document.querySelector("#content");
+// addEeventListener on the button
+searchButton.addEventListener("click", ()=>{
+    sendApiRequest();
+})
+
+// FETCH DE DATA FOM API
+
+async function sendApiRequest(){
+    let API_key = "chO7BLYLby497qh8f2SGKQ7x9SDQrfhAEd8HwEVX";
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_key}`);
+    console.log(response);
+    let data = await response.json();
+    useApiData(data);
+    console.log(data);
+}
+
+// what we do with de data 
+function useApiData(data){
+    content.innerHTML = data.explanation;
+    content.innerHTML += <img src="${data.explanation}"></img>
+}
